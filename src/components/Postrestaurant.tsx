4c6262463668f0f38a077c4a971c648ffd735f71
Conrelia Harder <cornelialisaharder@gmail.com>
2022-04-04 12:12:05 +0200

    with post new restaurant

---
+++ src/components/Postrestaurant.tsx
@@ -0,0 +1,25 @@
+import axios from "axios";
+
+export function Postrestaurant() {
+  //   axios
+  //     .post(
+  //       "https://school-restaurant-api.azurewebsites.net/restaurant/create",
+  //       {
+  //         name: "Husman",
+  //         address: {
+  //           street: "Åregatan 1",
+  //           zip: "123 45",
+  //           city: "Åre",
+  //         },
+  //       },
+  //       { headers: { "Content-Type": "application/json" } }
+  //     )
+  //     .then((response) => {
+  //       console.log(response);
+  //     });
+  return (
+    <>
+      <div></div>
+    </>
+  );
+}
