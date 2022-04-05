04a45c41906dfb963b4c80390b065a49a10d45d1
NikolaSalamurovic <94236794+NikolaSalamurovic@users.noreply.github.com>
2022-04-05 20:29:43 +0200

    BookingPage first big commit

--- src/components/Booking/Booking.tsx
+++ src/components/Booking/Booking.tsx
@@ -1,7 +1,143 @@
 import axios from "axios";
 import { useEffect } from "react";
+import React from "react";
+import { useState } from "react";
+import { IBooking } from "../../models/IBooking";
+import { ICustomer } from "../../models/ICustomer";
+import { IBookingUpload } from "../../models/IBookingUpload";
+import { stringify } from "querystring";
+
+
+
 
 export function Booking() {
+
+  const [selectedDate, setSelectedDate] = useState("");
+  const [bookingArray, setBookingArray] = useState<IBooking[]>([]);
+  const [availableBookings, setAvailableBookings] = useState<string[]>([]);
+  const [customerInfo, setCustomerInfo] = useState<IBookingUpload>({
+    restaurantId: "624ac35fdf8a9fb11c3ea8ba",
+    date: "",
+    time: "",
+    numberOfGuests: 0,
+    customer: {
+      name: "",
+      lastname: "",
+      email: "",
+      phone: "",
+    }
+  });
+
+  useEffect(() => {
+    axios
+      .get<IBooking[]>(
+        "https://school-restaurant-api.azurewebsites.net/booking/restaurant/624ac35fdf8a9fb11c3ea8ba",
+
+        { headers: { "Content-Type": "application/json" } }
+      )
+
+      .then((response) => {
+        setBookingArray(response.data);
+      });
+  }, []);
+  useEffect(() => {
+    renderTimeSlots();
+    setCustomerInfo({
+      restaurantId: "624ac35fdf8a9fb11c3ea8ba",
+      date: "",
+      time: "",
+      numberOfGuests: 0,
+      customer: {
+        name: "",
+        lastname: "",
+        email: "",
+        phone: "",
+      }})
+  }, [selectedDate]);
+
+  useEffect(() => {
+    console.log(customerInfo);
+  }, [customerInfo]);
+  
+  
+  function renderTimeSlots(){
+    let bookings = bookingArray.filter((booking) => booking.date === selectedDate)
+    let earlyBooking = bookings.filter((booking) => booking.time === "18:00");
+    let lateBooking = bookings.filter((booking) => booking.time === "21:00");
+
+    let tempArray = [];
+    for (let i = 0; 15-earlyBooking.length>i; i++){
+      tempArray.push("18:00");
+    }
+    for (let i = 0; 15-lateBooking.length>i; i++){
+      tempArray.push("21:00");
+    }
+    setAvailableBookings(tempArray);
+    
+  }
+  function uploadBooking(){
+    axios
+      .post(
+        "https://school-restaurant-api.azurewebsites.net/booking/create",
+
+        {
+          restaurantId: "624ac35fdf8a9fb11c3ea8ba",
+          date: customerInfo.date,
+          time: customerInfo.time,
+          numberOfGuests: customerInfo.numberOfGuests,
+          customer: {
+            name: customerInfo.customer.name,
+            lastname: customerInfo.customer.lastname,
+            email: customerInfo.customer.email,
+            phone: customerInfo.customer.phone,
+          },
+        },
+
+        { headers: { "Content-Type": "application/json" } }
+      )
+
+      .then((response) => {
+        console.log(response.data);
+      });
+  }
+  return (
+    <>
+      <input type="date" onChange={(e) => setSelectedDate(e.target.value)}></input>
+      <div>
+        
+        {selectedDate.length>0 && availableBookings.map((timeslot,i) => {
+          return(
+            <button key={i} onClick={() => setCustomerInfo({...customerInfo, time:timeslot, date:selectedDate})}>{timeslot}</button>
+          )
+        })}
+        { selectedDate.length>0 && customerInfo.time.length>0 &&
+          <>
+            <select name="" id="" onChange={(e) => setCustomerInfo({...customerInfo, numberOfGuests:+e.target.value})}>
+              <option value="1">1</option>
+              <option value="2">2</option>
+              <option value="3">3</option>
+              <option value="4">4</option>
+              <option value="5">5</option>
+              <option value="6">6</option>
+            </select>
+            <input type="text" placeholder="name" onChange={(e) => setCustomerInfo({...customerInfo, customer:{...customerInfo.customer, name:e.target.value}})}/>
+            <input type="text" placeholder="lastname" onChange={(e) => setCustomerInfo({...customerInfo, customer:{...customerInfo.customer, lastname:e.target.value}})}/>
+            <input type="tel" placeholder="phone" onChange={(e) => setCustomerInfo({...customerInfo, customer:{...customerInfo.customer, phone:e.target.value}})}/>
+            <input type="text" placeholder="email" onChange={(e) => setCustomerInfo({...customerInfo, customer:{...customerInfo.customer, email:e.target.value}})}/>
+          </>
+        }
+        <button onClick={uploadBooking}>Boka</button>
+      </div>
+    </>
+  )
+   
+
+}
+
+
+
+
+
   // useEffect(() => {
   //   axios
   //     .post(
@@ -12,32 +148,6 @@ export function Booking() {
   //         lastname: "Kurt",
   //         email: "someone@somedomain.com",
   //         phone: "070-0000000",
-  //       },
-
-  //       { headers: { "Content-Type": "application/json" } }
-  //     )
-
-  //     .then((response) => {
-  //       console.log(response.data);
-  //     });
-  // }, []);
-
-  // useEffect(() => {
-  //   axios
-  //     .post(
-  //       "https://school-restaurant-api.azurewebsites.net/booking/create",
-
-  //       {
-  //         restaurantId: "624ac35fdf8a9fb11c3ea8ba",
-  //         date: "2022-04-07",
-  //         time: "21:00",
-  //         numberOfGuests: 4,
-  //         customer: {
-  //           name: "Husman",
-  //           lastname: "Pontus",
-  //           email: "Marcus@somedomain.com",
-  //           phone: "070-0000111",
-  //         },
   //       },
 
   //       { headers: { "Content-Type": "application/json" } }
@@ -81,24 +191,5 @@ export function Booking() {
   //         console.log(response.data);
   //       });
   //   }, []);
+  
 
-  useEffect(() => {
-    axios
-      .get(
-        "https://school-restaurant-api.azurewebsites.net/booking/restaurant/624ac35fdf8a9fb11c3ea8ba",
-
-        { headers: { "Content-Type": "application/json" } }
-      )
-
-      .then((response) => {
-        console.log(response.data);
-      });
-  }, []);
-
-  return (
-    <div>
-      <select name="" id="">
-      </select>
-    </div>
-  )
-}
--- src/models/IBooking.ts
+++ src/models/IBooking.ts
@@ -1,7 +1,7 @@
 export interface IBooking {
   _id: string;
   restaurantId: "624ac35fdf8a9fb11c3ea8ba";
-  date: Date;
+  date: string;
   time: string;
   numberOfGuests: number;
   customer: string;
