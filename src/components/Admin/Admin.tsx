3c99cc525df926eac2b5088e4c16c62e264b4c69
Conrelia Harder <cornelialisaharder@gmail.com>
2022-04-05 16:09:01 +0200

    test pullrequest

--- src/components/Admin/Admin.tsx
+++ src/components/Admin/Admin.tsx
@@ -8,6 +8,7 @@ import { PrintBookingAdmin } from "./PrintBookingAdmin";
 export function Admin() {
   const [bookingArray, setBookingArray] = useState<IBooking[]>([]);
   const [deleteBoolean, setDeleteBoolean] = useState(false);
+  let number = 0;
   useEffect(() => {
     axios
       .get<IBooking[]>(
