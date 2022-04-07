82ae99f436c9009c1ce417bb7eae1b58191a0a9c
Hektor-Byfors <hektor.byfors@hotmail.com>
2022-04-07 18:15:06 +0200

    styled content

--- src/components/Contact/Contact.tsx
+++ src/components/Contact/Contact.tsx
@@ -1,4 +1,4 @@
-import { ChangeEvent, useState } from "react";
+import { ChangeEvent, useEffect, useState } from "react";
 import "./style.css";
 
 interface INewUserMessage {
@@ -9,31 +9,31 @@ interface INewUserMessage {
  }
 
 export function Contact() {
-  const [isMessageSent, setIsMessageSent] = useState(false)
+  const [isMessageSent, setIsMessageSent] = useState(false);
   const [newUserMessage, setNewUserMessage] = useState<INewUserMessage>({
     name: "",
     email: "",
     phone:"",
     message:""
-   })
+   });
   
    function handleInputChange(e: ChangeEvent<HTMLInputElement>) {
-    setNewUserMessage({...newUserMessage, [e.target.name]: e.target.value})
-   }
+    setNewUserMessage({...newUserMessage, [e.target.name]: e.target.value});
+   };
 
    function handleTextAreaChange(e: ChangeEvent<HTMLTextAreaElement>) {
     setNewUserMessage({...newUserMessage, [e.target.name]: e.target.value})
    }
 
    function sendMessage() {
-     if (
+    if (
        newUserMessage.name === "" ||
        newUserMessage.email === "" ||
        newUserMessage.phone === "" ||
        newUserMessage.message === ""
      ) return
      setIsMessageSent(!isMessageSent)
-   }
+   };
 
    function sendNewMessage() {
      setNewUserMessage({
@@ -43,18 +43,18 @@ export function Contact() {
       message:""
      })
      setIsMessageSent(!isMessageSent)
-   }
+   };
 
   return (
     <div className="container">
 
       {isMessageSent && 
         <div className="sentMessage">
-          <p>Tack för ditt meddelande! Vi återkopplar så snart som möjligt</p>
-          <p>Namn: {newUserMessage.name}</p>
-          <p>E-mail: {newUserMessage.email}</p>
-          <p>Telefon: {newUserMessage.phone}</p>
-          <p>Meddelande:<br/>{newUserMessage.message}</p>
+          <p className="bold">Tack för ditt meddelande!<br/>Vi återkopplar så snart som möjligt.</p>
+          <p><span className="boldUnderline">Namn:</span> {newUserMessage.name}</p>
+          <p><span className="boldUnderline">E-mail:</span> {newUserMessage.email}</p>
+          <p><span className="boldUnderline">Telefon:</span> {newUserMessage.phone}</p>
+          <p><span className="boldUnderline">Meddelande:</span><br/>{newUserMessage.message}</p>
           <button onClick={sendNewMessage}>Skicka ett till meddelande</button>
         </div>}
 
@@ -62,14 +62,21 @@ export function Contact() {
         <form className="form">
           <p className="requiredWarning">*Obligatoriskt fält*</p>
           <input type="text" placeholder="Namn" name="name" value={newUserMessage.name} onChange={handleInputChange}/>
+
+          <p className="requiredWarning">*Obligatoriskt fält*</p>
           <input type="text" placeholder="E-mail" name="email" value={newUserMessage.email} onChange={handleInputChange}/>
+
+          <p className="requiredWarning">*Obligatoriskt fält*</p>
           <input type="text" placeholder="Telefon" name="phone" value={newUserMessage.phone} onChange={handleInputChange}/>
-          <textarea className="messageBox"  placeholder="Meddelande" name="message" value={newUserMessage.message} onChange={handleTextAreaChange}/>
+
+          <p className="requiredWarning">*Obligatoriskt fält*</p>
+          <textarea placeholder="Meddelande" name="message" value={newUserMessage.message} onChange={handleTextAreaChange}/>
+
           <button onClick={sendMessage}>Skicka meddelande</button>
         </form>}
 
-      <div className="restaurantinfo">
-        <p>Husman</p>
+      <div className="restaurantInfo">
+        <p className="bold">Du hittar oss på:</p>
         <p>Åregatan 1</p>
         <p>123 45</p>
         <p>Åre</p>
