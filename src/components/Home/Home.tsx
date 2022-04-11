47021148387f0b6dfa58020ab24aff3b0b764ca1
Conrelia Harder <cornelialisaharder@gmail.com>
2022-04-11 13:14:35 +0200

    N, H & C moved asset-folder

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -1,14 +1,14 @@
 import "./Home.css";
-import image1 from "./assets/honey-gf7e3a6d22_1280.jpg";
-import image2 from "./assets/415598-svetik_1920x1080.jpg";
-import image3 from "./assets/spaghetti-g8ceb99e6c_1280.jpg";
-import image4 from "./assets/pexels-lisa-fotios-918327.jpg";
-import image5 from "./assets/5683239.jpg";
-import imagemo1 from "./assets/imagemo4.jpg";
-import imagemo2 from "./assets/imagemo2.jpg";
-import imagemo3 from "./assets/imagemo5.jpg";
-import imagemo4 from "./assets/415598-svetik_1920x1080 copy.jpg";
-import imagemo5 from "./assets/5683239 copy.jpg";
+import image1 from "../.././assets/honey-gf7e3a6d22_1280.jpg";
+import image2 from "../.././assets/415598-svetik_1920x1080.jpg";
+import image3 from "../.././assets/spaghetti-g8ceb99e6c_1280.jpg";
+import image4 from "../.././assets/pexels-lisa-fotios-918327.jpg";
+import image5 from "../.././assets/5683239.jpg";
+import imagemo1 from "../.././assets/imagemo4.jpg";
+import imagemo2 from "../.././assets/imagemo2.jpg";
+import imagemo3 from "../.././assets/imagemo5.jpg";
+import imagemo4 from "../.././assets/415598-svetik_1920x1080 copy.jpg";
+import imagemo5 from "../.././assets/5683239 copy.jpg";
 export function Home() {
   return (
     <>
