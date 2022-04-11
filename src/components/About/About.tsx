74eee48f08e9814e0cc7e6cfecdde765085685a4
Conrelia Harder <cornelialisaharder@gmail.com>
2022-04-11 17:25:46 +0200

    created about-page and added animations

--- src/components/About/About.tsx
+++ src/components/About/About.tsx
@@ -1,3 +1,37 @@
+import "./About.css";
+
 export function About() {
-  return <>About works.</>;
+  return (
+    <>
+      <div className="blurredBackground"></div>
+      <div className="containerAbout">
+        <section className="aboutCard">
+          <h2 className="aboutTitle">HUSMAN</h2>
+          <p className="aboutParagraph">
+            Koncept: Husman är en Åre-besökares dröm! Inrett för semester men
+            smakar som hemma! Konceptet är enkelt, fine-dining husmanskost som
+            är traditionellt lagad men modernt presenterat! Lägg till några
+            härliga cocktails till din mat eller drick din favorit-lager.
+          </p>
+          <p className="aboutEvents">Missa inte våra kommande event:</p>
+          <ul>
+            <li className="listItemEvents">
+              <h3>23/-2022</h3>
+              <p>After-ski med köttbullar och lingon!</p>
+            </li>
+            <li className="listItemEvents">
+              <h3>22/5-2022</h3>
+              <p>Schlager-Quiz-Night med pytt-i-panna och stekt ägg!</p>
+            </li>
+            <li className="listItemEvents">
+              <h3>2/6-2022</h3>
+              <p>
+                Singelmingel med Råraka och Happy Hour mellan kl.16 och kl.17!
+              </p>
+            </li>
+          </ul>
+        </section>
+      </div>
+    </>
+  );
 }
