68bbdd1ce37fe712cc4a7c71133bb4c306099c87
Hektor-Byfors <hektor.byfors@hotmail.com>
2022-04-19 13:04:47 +0200

    fixed footer placement bug

--- src/components/Booking/Booking.tsx
+++ src/components/Booking/Booking.tsx
@@ -174,95 +174,97 @@ export function Booking() {
 
   return (
     <>
-      <div className="backgroundPicture"></div>
-      <h1 className="bookingTitle">Bokning</h1>
-      <div className="bookingBorder">
-        <form onSubmit={onSubmit}
-        >
-          <input type="date" className="bookingDate" onClick={resetInputs} onChange={(e) => {setSelectedDate(e.target.value)}} ></input>
-          <div>
-            {selectedDate.length > 0 && (
-              <div className="timeDiv">
-                <select name="" id="selectedTime" onChange={(e) => setCustomerInfo({...customerInfo, time: e.target.value, date: selectedDate})}>
-                        {!(customerInfo.time === "18:00" ||customerInfo.time === "21:00") && 
-                        <option value={"0"}>Välj tid*</option>}
-                  {selectedDate.length > 0 && availableBookings.filter((timeslot) => timeslot === "18:00").length > 0 && (
-                        <option value={"18:00"}>18:00</option>
-                    )}
-                  {selectedDate.length > 0 && availableBookings.filter((timeslot) => timeslot === "21:00").length > 0 && (
-                        <option value={"21:00"}>21:00</option>
-                    )}
-                </select>
-              </div>  
-            )}
-
-            {selectedDate.length > 0 && (customerInfo.time=== "18:00" || customerInfo.time === "21:00") && (
-              <div>
-                <div className="formGuests">
-                  <select
-                    {...register("numberOfGuests", {required: true, min: 1,})} name="numberOfGuests" className="guestNumber" onChange={(e) =>
-                      setCustomerInfo({...customerInfo,numberOfGuests: +e.target.value,})
-                    }
-                  >
-                    <option value="0">Antal gäster*</option>
-                    <option value="1">1</option>
-                    <option value="2">2</option>
-                    <option value="3">3</option>
-                    <option value="4">4</option>
-                    <option value="5">5</option>
-                    <option value="6">6</option>
+      <div className="bookingContainer">
+        <div className="backgroundPicture"></div>
+        <h1 className="bookingTitle">Bokning</h1>
+        <div className="bookingBorder">
+          <form onSubmit={onSubmit}
+          >
+            <input type="date" className="bookingDate" onClick={resetInputs} onChange={(e) => {setSelectedDate(e.target.value)}} ></input>
+            <div>
+              {selectedDate.length > 0 && (
+                <div className="timeDiv">
+                  <select name="" id="selectedTime" onChange={(e) => setCustomerInfo({...customerInfo, time: e.target.value, date: selectedDate})}>
+                          {!(customerInfo.time === "18:00" ||customerInfo.time === "21:00") && 
+                          <option value={"0"}>Välj tid*</option>}
+                    {selectedDate.length > 0 && availableBookings.filter((timeslot) => timeslot === "18:00").length > 0 && (
+                          <option value={"18:00"}>18:00</option>
+                      )}
+                    {selectedDate.length > 0 && availableBookings.filter((timeslot) => timeslot === "21:00").length > 0 && (
+                          <option value={"21:00"}>21:00</option>
+                      )}
                   </select>
-                </div>
-                <div className="formInput">
-                  <StyledInput className="formInputs"
-                    type="text" {...register("firstName")} placeholder="Förnamn*" onChange={(e) =>
-                      setCustomerInfo({...customerInfo,customer: {...customerInfo.customer,name: e.target.value
-                        },
-                      })
-                    }
-                  />
-                  <StyledInput className="formInputs"
-                    type="text"{...register("lastName", {required: true, min: 4,})} placeholder="Efternamn*"onChange={(e) =>
-                      setCustomerInfo({...customerInfo,customer: {...customerInfo.customer,lastname: e.target.value,
-                        },
-                      })
-                    }
-                  />
-                  <StyledInput className="formInputs"
-                    type="tel" {...register("phone", {required: true})} placeholder="Telefon-nummer*"onChange={(e) =>
-                      setCustomerInfo({...customerInfo,customer: {...customerInfo.customer,phone: e.target.value,
-                        },
-                      })
-                    }
-                  />
-                  <StyledInput className="formInputs"
-                    type="email" {...register("email", {required: true})} placeholder="Email*" onChange={(e) =>
-                      {
-                      clickclick(); setCustomerInfo({...customerInfo, customer: {...customerInfo.customer, email: e.target.value,} ,
-                      })
-                    }
-                    }
-                  />
-                </div>
+                </div>  
+              )}
+
+              {selectedDate.length > 0 && (customerInfo.time=== "18:00" || customerInfo.time === "21:00") && (
                 <div>
-                  <input type="checkbox" className="gdprInput" onClick={checkedValid}/> Genom att klicka i rutan godkänner jag <a href="https://gdprinfo.eu/sv/sv-article-6" target="_blank">villkoren</a> och samtycker härmed att Husman lagrar de personliga uppgifterna skrivna ovan.* 
+                  <div className="formGuests">
+                    <select
+                      {...register("numberOfGuests", {required: true, min: 1,})} name="numberOfGuests" className="guestNumber" onChange={(e) =>
+                        setCustomerInfo({...customerInfo,numberOfGuests: +e.target.value,})
+                      }
+                    >
+                      <option value="0">Antal gäster*</option>
+                      <option value="1">1</option>
+                      <option value="2">2</option>
+                      <option value="3">3</option>
+                      <option value="4">4</option>
+                      <option value="5">5</option>
+                      <option value="6">6</option>
+                    </select>
+                  </div>
+                  <div className="formInput">
+                    <StyledInput className="formInputs"
+                      type="text" {...register("firstName")} placeholder="Förnamn*" onChange={(e) =>
+                        setCustomerInfo({...customerInfo,customer: {...customerInfo.customer,name: e.target.value
+                          },
+                        })
+                      }
+                    />
+                    <StyledInput className="formInputs"
+                      type="text"{...register("lastName", {required: true, min: 4,})} placeholder="Efternamn*"onChange={(e) =>
+                        setCustomerInfo({...customerInfo,customer: {...customerInfo.customer,lastname: e.target.value,
+                          },
+                        })
+                      }
+                    />
+                    <StyledInput className="formInputs"
+                      type="tel" {...register("phone", {required: true})} placeholder="Telefon-nummer*"onChange={(e) =>
+                        setCustomerInfo({...customerInfo,customer: {...customerInfo.customer,phone: e.target.value,
+                          },
+                        })
+                      }
+                    />
+                    <StyledInput className="formInputs"
+                      type="email" {...register("email", {required: true})} placeholder="Email*" onChange={(e) =>
+                        {
+                        clickclick(); setCustomerInfo({...customerInfo, customer: {...customerInfo.customer, email: e.target.value,} ,
+                        })
+                      }
+                      }
+                    />
+                  </div>
+                  <div>
+                    <input type="checkbox" className="gdprInput" onClick={checkedValid}/> Genom att klicka i rutan godkänner jag <a href="https://gdprinfo.eu/sv/sv-article-6" target="_blank">villkoren</a> och samtycker härmed att Husman lagrar de personliga uppgifterna skrivna ovan.* 
+                  </div>
+                    <button type="submit" className="submitBtn" 
+                    disabled={!customerInfo.customer.lastname || !customerInfo.customer.name || !customerInfo.customer.email || !customerInfo.customer.phone || !customerInfo.numberOfGuests || !buttonDisabled}
+                    onClick={uploadBooking}>
+                      Boka
+                    </button>
+                    <button className="cancelBtn" onClick={() => {
+                        reset();
+                        cancelBooking();
+                      }}
+                    >
+                      Avbryt
+                    </button>
                 </div>
-                  <button type="submit" className="submitBtn" 
-                  disabled={!customerInfo.customer.lastname || !customerInfo.customer.name || !customerInfo.customer.email || !customerInfo.customer.phone || !customerInfo.numberOfGuests || !buttonDisabled}
-                  onClick={uploadBooking}>
-                    Boka
-                  </button>
-                  <button className="cancelBtn" onClick={() => {
-                      reset();
-                      cancelBooking();
-                    }}
-                  >
-                    Avbryt
-                  </button>
-              </div>
-            )}
-          </div>
-        </form>
+              )}
+            </div>
+          </form>
+        </div>
       </div>
     </>
   );
