473069248d69f7c15a607dae53dcec55fb51187e
Hektor-Byfors <hektor.byfors@hotmail.com>
2022-04-19 13:14:00 +0200

    fixed merge errors

--- src/components/About/About.tsx
+++ src/components/About/About.tsx
@@ -3,7 +3,6 @@ import "./About.css";
 export function About() {
   return (
     <>
-<<<<<<< HEAD
       <div className="adminContainer">
         <div className="blurredBackground"></div>
         <div className="containerAbout">
@@ -34,36 +33,6 @@ export function About() {
             </ul>
           </section>
         </div>
-=======
-      <div className="blurredBackground"></div>
-      <div className="containerAbout">
-        <section className="aboutCard">
-          <h2 className="aboutTitle">HUSMAN</h2>
-          <p className="aboutParagraph">
-            Koncept: Husman är en Åre-besökares dröm! Inrett för semester men
-            smakar som hemma! Konceptet är enkelt, fine-dining husmanskost som
-            är traditionellt tillagad men modernt presenterad! Lägg till några
-            härliga cocktails till din mat eller drick din favorit-lager.
-          </p>
-          <p className="aboutEvents">Missa inte våra kommande event:</p>
-          <ul>
-            <li className="listItemEvents">
-              <h3>23/4-2022</h3>
-              <p>After-ski med köttbullar och lingon!</p>
-            </li>
-            <li className="listItemEvents">
-              <h3>22/5-2022</h3>
-              <p>Schlager-Quiz-Night med pytt-i-panna och stekt ägg!</p>
-            </li>
-            <li className="listItemEvents">
-              <h3>2/6-2022</h3>
-              <p>
-                Singelmingel med Råraka och Happy Hour mellan kl.16 och kl.17!
-              </p>
-            </li>
-          </ul>
-        </section>
->>>>>>> 13eac20afbf360a165e28cf6e5a10e158528d9a1
       </div>
     </>
   );
