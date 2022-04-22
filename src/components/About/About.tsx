54f54302cb7e5b3d6132c1d7ef298758b9767f8b
Conrelia Harder <cornelialisaharder@gmail.com>
2022-04-22 06:12:08 +0200

    changed about date

--- src/components/About/About.tsx
+++ src/components/About/About.tsx
@@ -17,7 +17,7 @@ export function About() {
             <p className="aboutEvents">Missa inte våra kommande event:</p>
             <ul>
               <li className="listItemEvents">
-                <h3>23/-2022</h3>
+                <h3>13/5-2022</h3>
                 <p>After-ski med köttbullar och lingon!</p>
               </li>
               <li className="listItemEvents">
